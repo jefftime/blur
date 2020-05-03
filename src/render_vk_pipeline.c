d43c9ba4e852f919b306ad0e466462d6b578cd60
Jeffery Stager <jffry09@gmail.com>
2020-05-03 11:24:11 -0400

    render: begin work on pipelines

---
+++ src/render_vk_pipeline.c
@@ -0,0 +1,16 @@
+#include "render.h"
+
+struct render_pipeline *render_create_pipeline(struct render *r) {
+  return NULL;
+}
+
+struct render_pipeline *render_create_pipelines(
+  struct render *r,
+  size_t n_pipelines
+) {
+  return NULL;
+}
+
+void render_del_pipeline(struct render_pipeline *rp) {
+  
+}
---
+++ validate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_core_validation:VK_LAYER_LUNARG_object_tracker ./run.sh
